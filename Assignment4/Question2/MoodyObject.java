d18823b801b61c75956b8d98f40d80da693350db
BharathiMadhu6 <55062688+BharathiMadhu6@users.noreply.github.com>
2019-10-07 23:22:55 -0700

    MoodyObject.java

---
+++ Assignment4/Question2/MoodyObject.java
@@ -0,0 +1,13 @@
+package Question2;
+
+public abstract class MoodyObject {
+
+    protected abstract String getMood();
+
+    protected abstract void expressFeelings();
+
+    public void queryMood() {
+        System.out.println(getMood());
+    }
+
+}
